c65848530ad200fc29c9a85812b5579cce4e57af
Jay Colson <jay@karma.net>
2021-04-08 09:33:52 +0100

    show in log if command was an interaction

--- bot.js
+++ bot.js
@@ -871,7 +871,7 @@ async function handleCommandExec(guildConfig, messageContentLowercase, msg, msgP
     }
     // console.debug('handled', handled);
     if (handled) {
-        console.log(`msg processed: ${msg.guild ? msg.guild.name : "DIRECT"}:${msg.author.tag}${msg.member ? "(" + msg.member.displayName + ")" : ""}:${messageContentLowercase}:${JSON.stringify(msgParms)}`);
+        console.log(`msg processed:${msg.interaction?'INTERACTION:':''} ${msg.guild ? msg.guild.name : "DIRECT"}:${msg.author.tag}${msg.member ? "(" + msg.member.displayName + ")" : ""}:${messageContentLowercase}:${JSON.stringify(msgParms)}`);
     }
     return handled;
 }
