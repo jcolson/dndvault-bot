1fe50544c1c2ae9772a9db5bac9c07580bd026b3
Jay Colson <jay@karma.net>
2021-04-13 19:24:43 +0100

    added test cases for checkIfCommandsChanged also moved relative functions to utils.js

--- __test__/utils.test.js
+++ __test__/utils.test.js
@@ -46,10 +46,72 @@ test(`isTrue`, () => {
 
 test('lengthOfEmbed', () => {
     let embed = new MessageEmbed()
-    .setColor(utils.COLORS.BLUE)
-    .setTitle(`Review Changes for Character:`)
-    .setURL('https://discord.js.org/')
-    .setAuthor('DND Vault')
-    .setDescription('Some Description');
+        .setColor(utils.COLORS.BLUE)
+        .setTitle(`Review Changes for Character:`)
+        .setURL('https://discord.js.org/')
+        .setAuthor('DND Vault')
+        .setDescription('Some Description');
     expect(utils.lengthOfEmbed(embed)).toBe(77);
+});
+
+const testCommands = {
+    "help": {
+        "name": "help",
+        "description": "Get help about D&D Vault Bot",
+        "slash": true
+    },
+    "show": {
+        "name": "show",
+        "description": "Show a user's character from the vault",
+        "slash": true,
+        "options": [{
+            "name": "character_id",
+            "description": "The Character ID from the `list` command",
+            "required": true,
+            "type": 3
+        }]
+    }
+}
+
+test(`checkIfCommandsChanged false`, () => {
+    let testCommandsA1 = JSON.parse(JSON.stringify(testCommands));
+    let testCommandsB1 = JSON.parse(JSON.stringify(testCommands));
+    let testCommandsA = utils.transformCommandsToDiscordFormat(testCommandsA1);
+    let testCommandsB = utils.transformCommandsToDiscordFormat(testCommandsB1);
+    expect(utils.checkIfCommandsChanged(testCommandsA, testCommandsB)).toBe(false);
+});
+
+test(`checkIfCommandsChanged A true`, () => {
+    let testCommandsA1 = JSON.parse(JSON.stringify(testCommands));
+    let testCommandsB1 = JSON.parse(JSON.stringify(testCommands));
+    testCommandsA1.boogers = {
+        "name": "stats",
+        "description": "Get statistics about bot",
+        "slash": true
+    };
+    let testCommandsA = utils.transformCommandsToDiscordFormat(testCommandsA1);
+    let testCommandsB = utils.transformCommandsToDiscordFormat(testCommandsB1);
+    expect(utils.checkIfCommandsChanged(testCommandsA, testCommandsB)).toBe(true);
+});
+
+test(`checkIfCommandsChanged A option true`, () => {
+    let testCommandsA1 = JSON.parse(JSON.stringify(testCommands));
+    let testCommandsB1 = JSON.parse(JSON.stringify(testCommands));
+    testCommandsA1.show.options[0].required = false;
+    let testCommandsA = utils.transformCommandsToDiscordFormat(testCommandsA1);
+    let testCommandsB = utils.transformCommandsToDiscordFormat(testCommandsB1);
+    expect(utils.checkIfCommandsChanged(testCommandsA, testCommandsB)).toBe(true);
+});
+
+test(`checkIfCommandsChanged B true`, () => {
+    let testCommandsA1 = JSON.parse(JSON.stringify(testCommands));
+    let testCommandsB1 = JSON.parse(JSON.stringify(testCommands));
+    testCommandsB1.boogers = {
+        "name": "stats",
+        "description": "Get statistics about bot",
+        "slash": true
+    }
+    let testCommandsA = utils.transformCommandsToDiscordFormat(testCommandsA1);
+    let testCommandsB = utils.transformCommandsToDiscordFormat(testCommandsB1);
+    expect(utils.checkIfCommandsChanged(testCommandsA, testCommandsB)).toBe(true);
 });
