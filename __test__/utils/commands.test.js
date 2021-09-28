9a9a3da927295dae734ca781820e9f64a2377b32
Jay Colson <jay@karma.net>
2021-09-28 17:00:38 +0100

    changing github trigger

--- __test__/utils/commands.test.js
+++ __test__/utils/commands.test.js
@@ -1,6 +1,10 @@
 const commands = require('../../utils/commands.js');
 const { testables } = commands;
 
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
 test('COMMANDS is not undefined or empty', () => {
     expect(testables.COMMANDS).not.toBe(undefined);
     expect(testables.COMMANDS).not.toBe(null);
